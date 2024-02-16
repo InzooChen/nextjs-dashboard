cb8b9e84b71956cc17a63e0ff592f6cc81956e14
inzoo <inzoo.chen@gmail.com>
2024-02-16 10:46:15 +0800

    ch8-9, 緩衝頁面 & 區塊緩衝顯示

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,13 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import { unstable_noStore as noStore } from 'next/cache';
 
-export default function Page() {
+
+export default async function Page() {
+    noStore();
     const pathname = usePathname();
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     return (
         <div>
             <h1>{pathname}</h1>
